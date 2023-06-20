e0c968c73bbdc0530eef1280cd541ba71bf8748e
Samara Mohsin <Samara Mohsin@DESKTOP-BP4905N>
2023-06-20 12:53:40 +0500

    web-forms work done till checkboxes

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,91 @@
+<template>
+  <form>
+    <Label>Email:</Label>
+    <input type="email" required v-model="email">
+
+    <Label>Password:</Label>
+    <input type="password" required v-model="password">
+
+    <label>Role</label>
+    <select v-model="role">
+        <option value="developer">Web Developer</option>
+        <option value="designer">Web Designer</option>
+    </select>
+
+    <div class="terms">
+        <input type="checkbox" v-model="terms" required>
+        <label>Accept Terms and Conditions</label>
+
+    </div>
+    <div>
+      <input type="checkbox" value="shaun" v-model="names">
+      <label>shaun</label>
+    </div>
+    <div>
+      <input type="checkbox" value="yoshi" v-model="names">
+      <label>yoshi</label>
+    </div>
+    <div>
+      <input type="checkbox" value="mario" v-model="names">
+      <label>mario</label>
+    </div>
+  </form>
+  <p>Email: {{ email }}</p>
+  <p>Password: {{ password }}</p>
+  <p>Role: {{ role }}</p>
+  <p>Terms accepted: {{ terms }}</p>
+  <p>Terms accepted: {{ names }}</p>
+
+
+
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            email: 'samara',
+            password:'',
+            role:'designer',
+            terms: true,
+            names : []
+        }
+    }
+}
+</script>
+
+<style>
+ form {
+    max-width: 420px;
+    margin: 30px auto;
+    background: white;
+    text-align: left;
+    padding: 40px;
+    border-radius: 10px;
+  }
+  label {
+    color: #aaa;
+    display: inline-block;
+    margin: 25px 0 15px;
+    font-size: 0.6em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: bold;
+  }
+  input,select {
+    display: block;
+    padding: 10px 6px;
+    width: 100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    color: #555;
+  }
+  input[type="checkbox"]{
+    display: inline-block;
+    width: 16px;
+    margin: 0 10px 0 0;
+    position: relative;
+    top: 2px;
+  }
+</style>
